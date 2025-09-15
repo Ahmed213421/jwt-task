2cb8244a500c3cfa0ad69cdb748fdd28fadd2fd6
Ahmed Samir <spiderofegypr98@gmail.com>
2025-09-15 15:28:01 +0300

    fix

--- app/Http/Controllers/Api/V1/UserAuthController.php
+++ app/Http/Controllers/Api/V1/UserAuthController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class UserAuthController extends Controller
